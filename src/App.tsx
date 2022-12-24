22443479c364c8af934ea125bffef63cddd9b3d2
Nikki NoLife <nikkinolife@gmail.com>
2022-12-24 09:45:32 +0400

    feat: add 404 error page and product card component stub

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,21 @@ import Cart from "./pages/cart/Cart";
 import Details from "./pages/details/Details";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Error404 from "./pages/404/Error404";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="wrapper">
-        <Header />
+        <Header title={"Online Store"} cartTotal={1234} />
 
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/details" element={<Details />} />
+          <Route path="/404" element={<Error404 />} />
 
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<Navigate to="/404" replace />} />
         </Routes>
 
         <Footer />
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,27 @@
 import "./Header.scss";
+import React, { Component } from "react";
 
-// TODO add type
-const Header = () => {
-  return (
-    <header className="header">
-      <ul className="header__nav">
-        <li className="nav__item">
-          <h1>online store</h1>
-        </li>
-        <li className="nav__item">Cart total</li>
-        <li className="nav__item">
-          <img src="./assets/images/shopping-cart.png" alt="" />
-          <a href="/cart">To cart</a>
-        </li>
-      </ul>
-    </header>
-  );
+// TODO add cart icon with counter of added products
+type HeaderProps = {
+  title: string;
+  cartTotal: number;
 };
 
-export default Header;
+export default class Header extends Component<HeaderProps> {
+  render() {
+    return (
+      <header className="header">
+        <ul className="header__nav">
+          <li className="nav__item">
+            <h1>{this.props.title}</h1>
+          </li>
+          <li className="nav__item">Cart total: {this.props.cartTotal}</li>
+          <li className="nav__item">
+            <img src="./assets/images/shopping-cart.png" alt="" />
+            <a href="/cart">To cart</a>
+          </li>
+        </ul>
+      </header>
+    );
+  }
+}
