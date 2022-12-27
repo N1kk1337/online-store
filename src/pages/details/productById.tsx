09d98055de112e4cdb3b7a62a0067796c126c021
shemet <shemetttttttt8@gmail.com>
2022-12-27 23:54:43 +0100

    feat: add data for details(bed variant)

--- src/pages/details/productById.tsx
+++ src/pages/details/productById.tsx
@@ -1,8 +1,8 @@
 import products from "../../assets/data/products";
 
-const getProductById = (id: string | number) => {
-  const serchId = products.filter(function (val) {
-    return val.id === id;
+const getProductById = (id: string | number | undefined) => {
+  const serchId = products.find(function (val) {
+    return val.id == id;
   });
   return serchId;
 };
