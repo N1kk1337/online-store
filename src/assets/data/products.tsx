ebf616449bbdbb78847b05ee8727614a9f8ef446
Nikki NoLife <nikkinolife@gmail.com>
2023-01-10 23:06:11 +0400

    Updates

--- src/assets/data/products.tsx
+++
@@ -1,6 +0,0 @@
-import data from "./products.json";
-import Product from "../model/product";
-
-const products: Array<Product> = data.products;
-
-export default products;
--- src/types/types.ts
+++
@@ -1,1 +0,0 @@
-export type SortOptions = "By Name" | "By Price";
