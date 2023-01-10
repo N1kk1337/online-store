2450053a30bdfce0bf8cd77aab4f8c49035d4f2e
Nikki NoLife <nikkinolife@gmail.com>
2023-01-10 23:59:06 +0400

    fix: add HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,17 @@ import Error404 from "./pages/404/Error404";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="wrapper">
-        <Header title={"Online Store"} cartTotal={1234} />
-        <Routes>
-          <Route path="/online-store/" element={<Main />} />
-          <Route
-            path="/online-store/details/:detailsId"
-            element={<Details />}
-          />
-          <Route path="/online-store/cart" element={<Cart />} />
-          <Route path="/online-store/404" element={<Error404 />} />
-          <Route
-            path="*"
-            element={<Navigate to="/online-store/404" replace />}
-          />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="wrapper">
+      <Header title={"Online Store"} cartTotal={1234} />
+      <Routes>
+        <Route path="/online-store/" element={<Main />} />
+        <Route path="/online-store/details/:detailsId" element={<Details />} />
+        <Route path="/online-store/cart" element={<Cart />} />
+        <Route path="/online-store/404" element={<Error404 />} />
+        <Route path="*" element={<Navigate to="/online-store/404" replace />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
 
