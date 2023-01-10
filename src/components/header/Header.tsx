997ce4f44b2f964ce5f61a226e24ca2df30b403e
Nikki NoLife <nikkinolife@gmail.com>
2023-01-11 00:30:26 +0400

    fix: header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,22 +8,22 @@ type HeaderProps = {
   cartTotal: number;
 };
 
-export default class Header extends Component<HeaderProps> {
-  navigate = useNavigate();
-  render() {
-    return (
-      <header className="header">
-        <ul className="header__nav">
-          <li className="nav__item">
-            <h1>{this.props.title}</h1>
-          </li>
-          <li className="nav__item">Cart total: {this.props.cartTotal}</li>
-          <li className="nav__item">
-            <img src="./assets/images/shopping-cart.png" alt="" />
-            <a onClick={() => this.navigate("/cart")}>To cart</a>
-          </li>
-        </ul>
-      </header>
-    );
-  }
-}
+const Header = ({ title, cartTotal }: HeaderProps) => {
+  const navigate = useNavigate();
+  return (
+    <header className="header">
+      <ul className="header__nav">
+        <li className="nav__item">
+          <h1>{title}</h1>
+        </li>
+        <li className="nav__item">Cart total: {cartTotal}</li>
+        <li className="nav__item">
+          <img src="./assets/images/shopping-cart.png" alt="" />
+          <button onClick={() => navigate("/cart")}>To cart</button>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
